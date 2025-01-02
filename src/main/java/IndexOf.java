201de158c6bebc5139a0e708e950698758c348a5
Brian Yang <yangbri1@msu.edu>
2025-01-02 21:01:59 +0000

    Find first instance of given int 'n' within array of int 'arr' --- similar to String.indexOf() method when attempting to find first instance of some char within a String

--- src/main/java/IndexOf.java
+++ src/main/java/IndexOf.java
@@ -11,6 +11,15 @@ public class IndexOf {
      * @return the first index (position) of n in arr. if n does not exist in arr, return -1.
      */
     public int getIndex(int[] arr, int n){
+        // iterate through given array of int 'arr'
+        for(int i = 0; i < arr.length; ++i){
+            // if given int 'n' is found within 'arr'
+            if(arr[i] == n){
+                // return the index (& exit out of loop -- ends function)
+                return i;
+            }
+        }
+        // otherwise return -1
         return -1;
     }
 }
