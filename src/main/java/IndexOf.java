76d8eaaeeded06ee9a7da347128d1f957a43bba2
Brian Yang <yangbri1@msu.edu>
2025-01-02 21:17:12 +0000

    Convert the array of int 'arr' into a String of 'arr' elements concatenated together. Next, change given int 'n' to String type as later using built-in String.indexOf() parameter does NOT take int

--- src/main/java/IndexOf.java
+++ src/main/java/IndexOf.java
@@ -12,14 +12,34 @@ public class IndexOf {
      */
     public int getIndex(int[] arr, int n){
         // iterate through given array of int 'arr'
-        for(int i = 0; i < arr.length; ++i){
-            // if given int 'n' is found within 'arr'
-            if(arr[i] == n){
-                // return the index (& exit out of loop -- ends function)
-                return i;
-            }
+        // for(int i = 0; i < arr.length; ++i){
+        //     // if given int 'n' is found within 'arr'
+        //     if(arr[i] == n){
+        //         // return the index (& exit out of loop -- ends function)
+        //         return i;
+        //     }
+        // }
+        // // otherwise return -1
+        // return -1;
+
+        // declare empty String to later append values of 'arr' in String format
+        String emptyStr = "";
+
+        // initialize i to be iterator for loop below
+        int i = 0;
+        // iterate through given array of int 'arr'
+        while(i < arr.length){
+            // https://www.geeksforgeeks.org/different-ways-for-integer-to-string-conversions-in-java/
+            // used Java.util package built-in String.valueOf() method to type-cast from int to String
+            // alternative: use wrapper class Integer.toString() method
+            emptyStr += String.valueOf(arr[i]);
+            // increment by 1
+            ++i;
         }
-        // otherwise return -1
-        return -1;
+        // type-cast int 'n' to String type as .indexOf() parameter does NOT take in int type for comparison itself
+        String nStr = Integer.toString(n);
+        // employ built-in String.indexOf() method to index of first instance where given  
+        return(emptyStr.indexOf(nStr));
+
     }
 }
